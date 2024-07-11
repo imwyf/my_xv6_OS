9f431ea8e9f60efd10a5a1ba6604cc16346b5bc7
imwyf <1185095602@qq.com>
2024-07-11 20:59:10 +0800

    tmp

--- inc/kernel_mem.h
+++ inc/kernel_mem.h
@@ -3,29 +3,36 @@
 
 #include "inc/mmu.h"
 #include "inc/types.h"
+#include "inc/list.h"
 
-#define MAX_ORDER 11 // 内核管理的最大的连续物理块为2^(11-1) = 4MB
-#define ORDER_SIZE(o) (1 << (o)) // 2^o，计算o阶的内存块大小
+#define MAX_ORDER 11 // 内核管理的最大的连续物理块为2^10页 = 4MB
+// #define MEM2ORDER(size) (ROUNDUP(size)) // 计算内存块的阶
 
+/* 下面是管理物理内核页的结构： */
 /**
- * 一个Page代表一个内存页
+ * 一个Page映射一个内存页
  */
 struct Page {
-    struct Page* pg_next; // next指针
+    struct list_head lru; // 用来链接进空闲链表的指针
     uint16_t pg_ref; // 本页的引用计数
+    uint32_t index; // 该页在整个内存空间的偏移
 };
 static struct Page* pages; // 物理内存页的数组
 static size_t n_page; // 物理内存页的数量
 
+/**
+ * 不同阶数的空闲链表
+ */
 struct free_list {
-    struct Page* head; // 指向空闲链表的头
-    uint32_t order; // 阶数
+    struct list_head list_head[1]; // 表头节点
+    uint32_t n_block; // 链表元素（内存块）个数
+    uint8_t order; // 本链表的阶数
 };
 
 typedef struct {
     // struct spinlock lock;
     // int use_lock;
-    struct free_list free_lists[MAX_ORDER - 1]; // 每个 free_lists[i] 中存放的是内存大小为 2^i 的空闲链表
+    struct free_list free_lists[MAX_ORDER]; // 每个 free_lists[i] 中存放的是内存大小为 2^i 的空闲链表
 } kmem_manager; // 内核内存管理器
 
 pde_t* kernel_pgdir;
@@ -38,11 +45,17 @@ struct Page* alloc_pages(uint32_t n);
 /**
  * 分配一个内存页，order指定阶数
  */
-struct Page* alloc_onepage(uint32_t order);
+struct Page* alloc_page(uint32_t order);
 /**
  * 释放p开始的n个内存页
  */
 void free_page(struct Page* p);
+
+void page_init();
+
+
+
+
 
 /**
  * 将物理页的偏移转换成物理地址pa
@@ -53,6 +66,14 @@ page2pa(struct Page* page)
     return (page - pages) << PGSHIFT;
 }
 /**
+ * 将物理页的偏移转换成内核虚拟地址kva
+ */
+static inline void*
+page2kva(struct Page* page)
+{
+    return K_P2V(page2pa(page));
+}
+/**
  * 将物理地址pa转换成物理页的偏移
  */
 static inline struct Page*
@@ -61,14 +82,6 @@ pa2page(paddr_t pa)
     // if (PGNUM(pa) >= n_page)
     //     panic("pa2page called with invalid pa");
     return &pages[PGNUM(pa)];
-}
-/**
- * 将物理页的偏移转换成内核虚拟地址kva
- */
-static inline void*
-page2kva(struct Page* page)
-{
-    return K_P2V(page2pa(page));
 }
 
 #endif // _KERNEL_MEM_H
--- kernel_mem.c
+++ kernel_mem.c
@@ -21,16 +21,24 @@ void kmem_init()
     memset(edata, 0, end - edata); // 先将bss段清零，确保所有静态/全局变量从零开始
 
     /* 现在的 entry_pgdir 只映射了低4MB内存，不够用，下面重新设置页表 */
-    memset(end, 0, K_P2V_WO(P_ADDR_LOWMEM)); // 由于只映射了低4MB内存，先初始化[end, 4MB]的空间来为新的页表腾出空间
+    memset(end, 0, K_P2V_WO(P_ADDR_LOWMEM)); // 由于只映射了低 4MB 内存，先初始化 [end, 4MB] 的空间来为新的页表腾出空间
     kernel_pgdir = (pde_t*)tmp_alloc(PGSIZE); // 分配一页内存作为页目录
+    kern_pgdir[PDX(UVPT)] = PADDR(kern_pgdir) | PTE_U | PTE_P;     // 递归地将PD作为页表插入其自身，以在虚拟地址UVPT处形成页表。
+
+    /* 伙伴系统初始化（页分配器），之后只能使用该系统管理内存 */
+    page_init();
 }
 
-// 仅在设置页表时使用的简单的物理内存分配器，之后使用alloc_page()分配
-// 分配一个足以容纳n字节的内存区间：用一个地址nextfree来确定可以使用的内存的顶部，并且返回可以使用的内存的底部地址result
-// 可使用内存区间为[result, nextfree], 且区间长度是4096的倍数
+void 
+
+/**
+ * 仅在设置页表时使用的简单的物理内存分配器，之后使用alloc_pages()分配,
+ * 分配一个足以容纳n字节的内存区间：用一个地址nextfree来确定可以使用的内存的顶部，并且返回可以使用的内存的底部地址result
+ * 可使用内存区间为[result, nextfree], 且区间长度是页对齐的
+ */
 static void* tmp_alloc(uint32_t n)
 {
-    static char* nextfree; // static意味着nextfree不会随着函数返回被重置，是全局变量
+    static char* nextfree; // static意味着nextfree不会随着函数返回被重置，是静态变量
     char* result;
 
     if (!nextfree) // nextfree初始化，只有第一次运行会执行
@@ -105,4 +113,13 @@ char* alloc_page(char* vaddr)
     // if (kmem.use_lock)
     //     release(&kmem.lock);
     return (char*)r;
+}
+
+void page_init()
+{
+    /* 先为 pages 分配空间，以便映射每一页物理内存 */
+	pages = (struct Page*)tmp_alloc(n_page * sizeof(struct Page));
+	memset(pages, 0, n_page * sizeof(struct Page));
+
+    /* 将可用内存全部标记为空闲内存，并添加进伙伴系统的空闲链表中 */
 }
